c391d6f582916a34fefa40ea09b8b77b9e12abcb
Yonghua22 <yfyfjade@gmail.com>
2024-10-17 15:18:34 -0700

    add ingredients page

--- app/src/main/java/com/example/mobileproject/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/mobileproject/RecipeDetailsActivity.java
@@ -1,15 +1,29 @@
 package com.example.mobileproject;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobileproject.Models.RecipeDetailsResponse;
+import com.squareup.picasso.Picasso;
+
+import Listeners.RecipeDetailsListener;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
     int id;
+    TextView textView_meal_name, textView_meal_source,textView_meal_summary;
+    ImageView imageView_meal_image;
+    RecyclerView recycler_meal_ingredients;
+    RequestManager manager;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,36 @@ public class RecipeDetailsActivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        findViews();
         id = Integer.parseInt(getIntent().getStringExtra("id"));
+        manager = new RequestManager(this);
+        manager.getRecipeDetails(recipeDetailsListener,id);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Loading Details...");
+        dialog.show();
     }
+
+    private void findViews() {
+        textView_meal_name = findViewById(R.id.textView_meal_name);
+        textView_meal_source = findViewById(R.id.textView_meal_source);
+        textView_meal_summary = findViewById(R.id.textView_meal_summary);
+        imageView_meal_image = findViewById(R.id.imageView_meal_image);
+        recycler_meal_ingredients = findViewById(R.id.recycler_meal_ingredients);
+    }
+    private final RecipeDetailsListener recipeDetailsListener = new RecipeDetailsListener() {
+        @Override
+        public void didFetch(RecipeDetailsResponse response, String message) {
+            dialog.dismiss();
+            textView_meal_name.setText(response.title);
+            textView_meal_source.setText(response.sourceName);
+            textView_meal_summary.setText(response.summary);
+            Picasso.get().load(response.image).into(imageView_meal_image);
+
+        }
+
+        @Override
+        public void didError(String message) {
+
+        }
+    };
 }
